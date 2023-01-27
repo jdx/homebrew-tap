87fcb5c1567a3fbb3f0f5fd05efc3752593aa6b7
RTX <123107610+rtx-vm@users.noreply.github.com>
2023-01-27 01:09:28 +0000

    rtx v0.0.0-alpha.4

--- rtx.rb
+++ rtx.rb
@@ -2,20 +2,20 @@ class Rtx < Formula
   desc "Multi-language runtime manager"
   homepage "https://github.com/jdxcode/rtx"
   license "MIT"
-  version "0.0.0-alpha.3"
+  version "0.0.0-alpha.4"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jdxcode/rtx/releases/download/v0.0.0-alpha.3/rtx-v0.0.0-alpha.3-macos-x64.tar.xz"
-      sha256 "9c73971cd0bfc43388af60ca26ef2f5757c97731f16d40ffce182c234d61533f"
+      url "https://github.com/jdxcode/rtx/releases/download/v0.0.0-alpha.4/rtx-v0.0.0-alpha.4-macos-x64.tar.xz"
+      sha256 "08247ad23a69b08e9297a7c9433105a8a45de335d58ee751451688cf83d02784"
 
       def install
         bin.install "bin/rtx"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/jdxcode/rtx/releases/download/v0.0.0-alpha.3/rtx-v0.0.0-alpha.3-macos-arm64.tar.xz"
-      sha256 "06d1a2b9913a32a3df66cc39eb5f9cda97e4c83c53d9c9d1ba28dea6ae1cd8ca"
+      url "https://github.com/jdxcode/rtx/releases/download/v0.0.0-alpha.4/rtx-v0.0.0-alpha.4-macos-arm64.tar.xz"
+      sha256 "5dfdfc34052104dac6fe8e62b491143023099208e2eee515e34e8eb0a7ed6b9b"
 
       def install
         bin.install "bin/rtx"
@@ -25,16 +25,16 @@ class Rtx < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jdxcode/rtx/releases/download/v0.0.0-alpha.3/rtx-v0.0.0-alpha.3-linux-arm64.tar.xz"
-      sha256 "18ee334bf6325ecf104d775b6dfa04c43800f7cd5a133d646be4debea26d5a8b"
+      url "https://github.com/jdxcode/rtx/releases/download/v0.0.0-alpha.4/rtx-v0.0.0-alpha.4-linux-arm64.tar.xz"
+      sha256 "1509f838906d0d6dac1afa158f6b6594bf267dce155f118be319977d4742a1a2"
 
       def install
         bin.install "bin/rtx"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/jdxcode/rtx/releases/download/v0.0.0-alpha.3/rtx-v0.0.0-alpha.3-linux-x64.tar.xz"
-      sha256 "b6c12ab5a02489bf96146fa5694515b58e37a0cd1db860230b218f1d4dfc7b5f"
+      url "https://github.com/jdxcode/rtx/releases/download/v0.0.0-alpha.4/rtx-v0.0.0-alpha.4-linux-x64.tar.xz"
+      sha256 "11cb66bce7e2f286cc97ab3e5f0ce124ad910004b0573e488706619d7f0d0312"
 
       def install
         bin.install "bin/rtx"
