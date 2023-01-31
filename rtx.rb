7fc2b6cf93fcd5683e374552fbca194501933b9c
RTX <123107610+rtx-vm@users.noreply.github.com>
2023-01-31 18:09:40 +0000

    rtx v1.2.8

--- rtx.rb
+++ rtx.rb
@@ -2,20 +2,20 @@ class Rtx < Formula
   desc "Multi-language runtime manager"
   homepage "https://github.com/jdxcode/rtx"
   license "MIT"
-  version "1.2.7"
+  version "1.2.8"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.2.7/rtx-v1.2.7-macos-x64.tar.xz"
-      sha256 "0e08f0b927669dc106fca5100f0c037c8c15b068187503398d864d1612509422"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.2.8/rtx-v1.2.8-macos-x64.tar.xz"
+      sha256 "1fa018b66e6b1e7ea0fc3c033087a8c874e930e6017b12b4061d2ba1811742ae"
 
       def install
         bin.install "bin/rtx"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.2.7/rtx-v1.2.7-macos-arm64.tar.xz"
-      sha256 "6f021a954e138a723e9e884198e07c09ac1dc90785247f980938e13bc7732fec"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.2.8/rtx-v1.2.8-macos-arm64.tar.xz"
+      sha256 "1b98866bb801b518cf69c51dc1cc3e7bdb8022002dcde8f162bea35d078f34fe"
 
       def install
         bin.install "bin/rtx"
@@ -25,16 +25,16 @@ class Rtx < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.2.7/rtx-v1.2.7-linux-arm64.tar.xz"
-      sha256 "9ab1af102e9b6218dffe1390df38a3464e6e52662d859c4b149a70de897903e3"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.2.8/rtx-v1.2.8-linux-arm64.tar.xz"
+      sha256 "b49e168bfe52bd477484a79c12e132f23fafd09d2af4ad7c6eb4db1b87d8367f"
 
       def install
         bin.install "bin/rtx"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.2.7/rtx-v1.2.7-linux-x64.tar.xz"
-      sha256 "7c58bb30505e3e48df26c991492afff24bdce5540e21c76af9c76c048227e251"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.2.8/rtx-v1.2.8-linux-x64.tar.xz"
+      sha256 "183ce1fadae3e5aa3fb7f1617def8d46bd7db1dad59c0cb21f76130bb7df57e9"
 
       def install
         bin.install "bin/rtx"
