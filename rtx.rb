ed366fc8a0c36786c840db1c234d9cfc686182c2
RTX <123107610+rtx-vm@users.noreply.github.com>
2023-02-22 21:13:13 +0000

    rtx v1.15.0

--- rtx.rb
+++ rtx.rb
@@ -2,20 +2,20 @@ class Rtx < Formula
   desc "Multi-language runtime manager"
   homepage "https://github.com/jdxcode/rtx"
   license "MIT"
-  version "1.14.3"
+  version "1.15.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.14.3/rtx-v1.14.3-macos-x64.tar.xz"
-      sha256 "08373c93f216c1bfce1808cbb6fc895eefa5c415cf964cc7b0d5bb2ec0f75882"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.15.0/rtx-v1.15.0-macos-x64.tar.xz"
+      sha256 "2aa41f7e7504df7b8ea91612e9b9c1e66602901160b1fd332237c6343d78638a"
 
       def install
         bin.install "bin/rtx"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.14.3/rtx-v1.14.3-macos-arm64.tar.xz"
-      sha256 "19161e5fc0edbba612e97f9203bfe25525a3e59e8ffe72495a002ba4c8018b85"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.15.0/rtx-v1.15.0-macos-arm64.tar.xz"
+      sha256 "bab7a3e330509c5c187759c2e5bc60c7085e1146af269e31534ec75e421fbbaa"
 
       def install
         bin.install "bin/rtx"
@@ -25,16 +25,16 @@ class Rtx < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.14.3/rtx-v1.14.3-linux-arm64.tar.xz"
-      sha256 "b8dc8c08667b0e046e5c54609ce860057115b25f8572c097df5e1c581b17c008"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.15.0/rtx-v1.15.0-linux-arm64.tar.xz"
+      sha256 "943cfc934a6818a8d7541007640d0aa26bfe2bd696bd83df073377f240ab7400"
 
       def install
         bin.install "bin/rtx"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.14.3/rtx-v1.14.3-linux-x64.tar.xz"
-      sha256 "71b60e134c05dc4066af74e3a608ffa644691bb175b28a1bdd6e45b6d54b1561"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.15.0/rtx-v1.15.0-linux-x64.tar.xz"
+      sha256 "f6c6a0e75d05dc61091828a66771c1a7a78887541423724d5c6339105bbacf71"
 
       def install
         bin.install "bin/rtx"
