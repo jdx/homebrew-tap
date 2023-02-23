66bd23ef373d531b8eef6042f131932aa6f42f5c
RTX <123107610+rtx-vm@users.noreply.github.com>
2023-02-23 02:49:13 +0000

    rtx v1.15.1

--- rtx.rb
+++ rtx.rb
@@ -2,20 +2,20 @@ class Rtx < Formula
   desc "Multi-language runtime manager"
   homepage "https://github.com/jdxcode/rtx"
   license "MIT"
-  version "1.15.0"
+  version "1.15.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.15.0/rtx-v1.15.0-macos-x64.tar.xz"
-      sha256 "2aa41f7e7504df7b8ea91612e9b9c1e66602901160b1fd332237c6343d78638a"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.15.1/rtx-brew-v1.15.1-macos-x64.tar.xz"
+      sha256 "5990990960ec12a976a9872ff55033f28741f0da2f1f3ef3074c0e408583b859"
 
       def install
         bin.install "bin/rtx"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.15.0/rtx-v1.15.0-macos-arm64.tar.xz"
-      sha256 "bab7a3e330509c5c187759c2e5bc60c7085e1146af269e31534ec75e421fbbaa"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.15.1/rtx-brew-v1.15.1-macos-arm64.tar.xz"
+      sha256 "26ca6ca233f4761884f8b83e8a67754b6e2c2c3930836dcb9e91a1a014516bdc"
 
       def install
         bin.install "bin/rtx"
@@ -25,16 +25,16 @@ class Rtx < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.15.0/rtx-v1.15.0-linux-arm64.tar.xz"
-      sha256 "943cfc934a6818a8d7541007640d0aa26bfe2bd696bd83df073377f240ab7400"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.15.1/rtx-brew-v1.15.1-linux-arm64.tar.xz"
+      sha256 "82b41061fb3a47cf77873657eb4a28d3600e4873b6e714779cd27f82e8a517ba"
 
       def install
         bin.install "bin/rtx"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/jdxcode/rtx/releases/download/v1.15.0/rtx-v1.15.0-linux-x64.tar.xz"
-      sha256 "f6c6a0e75d05dc61091828a66771c1a7a78887541423724d5c6339105bbacf71"
+      url "https://github.com/jdxcode/rtx/releases/download/v1.15.1/rtx-brew-v1.15.1-linux-x64.tar.xz"
+      sha256 "169b2debb9b34c39e9fa68a23ad487932b0f13edf5a88dca890a67431cc76d83"
 
       def install
         bin.install "bin/rtx"
