a09001338ff52ef03aa51e679f7ca5729f558595
RTX <123107610+rtx-vm@users.noreply.github.com>
2023-04-20 03:12:28 +0000

    rtx 1.28.6

--- rtx.rb
+++ rtx.rb
@@ -2,27 +2,27 @@ class Rtx < Formula
   desc "Multi-language runtime manager"
   homepage "https://github.com/jdxcode/rtx"
   license "MIT"
-  version "1.28.5"
+  version "1.28.6"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://rtx.pub/v1.28.5/rtx-brew-v1.28.5-macos-x64.tar.xz"
-      sha256 "8b93a0fe8b7a7fcfdd9c2459f0522c8f2c1813496cd12acb6f815724456f8ddb"
+      url "https://rtx.pub/v1.28.6/rtx-brew-v1.28.6-macos-x64.tar.xz"
+      sha256 "db1544b3969c9636a417b7dea5acb7df3812558cf23c1a6842a00743ea0f8d5b"
     end
     if Hardware::CPU.arm?
-      url "https://rtx.pub/v1.28.5/rtx-brew-v1.28.5-macos-arm64.tar.xz"
-      sha256 "748f02c6c466e0be9994699628725645b8af60448a1f92e50a4a6ebf5725111e"
+      url "https://rtx.pub/v1.28.6/rtx-brew-v1.28.6-macos-arm64.tar.xz"
+      sha256 "257c7045bfa5cb410e6e2c76b80f4c7f8027e379d9a633e617ae53974d4d1d8c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://rtx.pub/v1.28.5/rtx-brew-v1.28.5-linux-arm64.tar.xz"
-      sha256 "ab13d9e0c17c0ba3e2866aca1889e68cfcf29d33aecd6e6ea3966dbb804b485b"
+      url "https://rtx.pub/v1.28.6/rtx-brew-v1.28.6-linux-arm64.tar.xz"
+      sha256 "2c93cf5c0e1ec7beec4cc158deacd7bc74a9d33c5da67d88e660c12a45c575cb"
     end
     if Hardware::CPU.intel?
-      url "https://rtx.pub/v1.28.5/rtx-brew-v1.28.5-linux-x64.tar.xz"
-      sha256 "f484cbd2d18e08dc4d7980af5d5b14674f3fcd2bc01f2790ba64da02d4c71b85"
+      url "https://rtx.pub/v1.28.6/rtx-brew-v1.28.6-linux-x64.tar.xz"
+      sha256 "6acef1b9aaf832384b1d29bdd7110f129750e6bf223489fb9e7762e470b39fc3"
     end
   end
 
@@ -34,7 +34,7 @@ class Rtx < Formula
 
   test do
     system "#{bin}/rtx --version"
-    system "#{bin}/rtx", "install", "nodejs@18.13.0"
-    assert_match "v18.13.0", shell_output("#{bin}/rtx exec nodejs@18.13.0 -- node -v")
+    system "#{bin}/rtx", "install", "nodejs@20.0.0"
+    assert_match "v20.0.0", shell_output("#{bin}/rtx exec nodejs@20.0.0 -- node -v")
   end
 end
