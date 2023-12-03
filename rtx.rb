7b55420567ef8b6176d0a49545aa0a1f5a9d1627
RTX <123107610+rtx-vm@users.noreply.github.com>
2023-12-03 23:46:54 +0000

    rtx 2023.12.7

--- rtx.rb
+++ rtx.rb
@@ -2,35 +2,36 @@ class Rtx < Formula
   desc "Multi-language runtime manager"
   homepage "https://github.com/jdx/rtx"
   license "MIT"
-  version "2023.12.6"
+  version "2023.12.7"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://rtx.pub/v2023.12.6/rtx-v2023.12.6-macos-x64.tar.xz"
-      sha256 "fa93dfa4cc864dec25b8662cbdfe5bb2c76e8ce2968d30ca4de58731b2dd3cac"
+      url "https://rtx.pub/v2023.12.7/rtx-v2023.12.7-macos-x64.tar.xz"
+      sha256 "ab0dffaa9bf2954cc17da27b958f47d215b203214e8055f406abe4dc639b2bd4"
     end
     if Hardware::CPU.arm?
-      url "https://rtx.pub/v2023.12.6/rtx-v2023.12.6-macos-arm64.tar.xz"
-      sha256 "11fcf76d809cea4a9c1e93338f2984059c988d166c356f198a59eaf6b8b6e8f2"
+      url "https://rtx.pub/v2023.12.7/rtx-v2023.12.7-macos-arm64.tar.xz"
+      sha256 "485983b4767c37a96f0d99e20d2657d0935089007477b74b5cf60c4a3678dc65"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://rtx.pub/v2023.12.6/rtx-v2023.12.6-linux-arm64.tar.xz"
-      sha256 "3265cbc5d115fa7a333a307a1be04e3c72c38fba81054496eba8a0286c5e7278"
+      url "https://rtx.pub/v2023.12.7/rtx-v2023.12.7-linux-arm64.tar.xz"
+      sha256 "3b087802e0d534c14f8d807c1c1791abcb846c8cab594a26bcc821d4be88b4aa"
     end
     if Hardware::CPU.intel?
-      url "https://rtx.pub/v2023.12.6/rtx-v2023.12.6-linux-x64.tar.xz"
-      sha256 "f4e7f0cbb7131289b612a6a65f507bd89ed165af71ac8417751ddef0bbf39ad6"
+      url "https://rtx.pub/v2023.12.7/rtx-v2023.12.7-linux-x64.tar.xz"
+      sha256 "26fa333823dd91412ff40e50072e216884ab9ba52fa762131fa4ad56172afdc5"
     end
   end
 
   def install
     bin.install "bin/rtx"
-    lib.write ".disable-self-update"
     man1.install "man/man1/rtx.1"
     generate_completions_from_executable(bin/"rtx", "completion")
+    lib.mkpath
+    touch lib/".disable-self-update"
   end
 
   test do
