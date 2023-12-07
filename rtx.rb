5733138d00ab2510cd8b37981602e474747ca5bb
RTX <123107610+rtx-vm@users.noreply.github.com>
2023-12-07 05:39:24 +0000

    rtx 2023.12.17

--- rtx.rb
+++ rtx.rb
@@ -1,37 +1,39 @@
+# frozen_string_literal: true
+
 class Rtx < Formula
   desc "Multi-language runtime manager"
   homepage "https://github.com/jdx/rtx"
   license "MIT"
-  version "2023.12.9"
+  version "2023.12.17"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://rtx.pub/v2023.12.9/rtx-v2023.12.9-macos-x64.tar.xz"
-      sha256 "5bc8b6abad2ac10f6516a2b2e5f8aba8292d13b177e6c8d96bcff0399d230220"
+      url "https://rtx.pub/v2023.12.17/rtx-v2023.12.17-macos-x64.tar.xz"
+      sha256 "4a143841aa9b7f1be534cc08648756995e472e23d682e4c13a860ed3bdf9b68d"
     end
     if Hardware::CPU.arm?
-      url "https://rtx.pub/v2023.12.9/rtx-v2023.12.9-macos-arm64.tar.xz"
-      sha256 "42b01db2fc2225c05cb7463958b19486a9aa2cfb172dcbf3dfa30cec2185ebed"
+      url "https://rtx.pub/v2023.12.17/rtx-v2023.12.17-macos-arm64.tar.xz"
+      sha256 "b165ae7b97c92d67c76161440bd4db2ee33ce660bba63a4347ab0e32acc6478d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://rtx.pub/v2023.12.9/rtx-v2023.12.9-linux-arm64.tar.xz"
-      sha256 "22693c354b41d42661990abd9d2cef60a244be5ca7a53c470952dcce1ad586cf"
+      url "https://rtx.pub/v2023.12.17/rtx-v2023.12.17-linux-arm64.tar.xz"
+      sha256 "4cc7ff55151de1c759d7c46c645bc2eda3ad44b3cec6655eef7e679eb800536f"
     end
     if Hardware::CPU.intel?
-      url "https://rtx.pub/v2023.12.9/rtx-v2023.12.9-linux-x64.tar.xz"
-      sha256 "c5f3b98161877e12a25a4c78e85ae33cf042fcd855c0ff0bcf86394e6f5b1886"
+      url "https://rtx.pub/v2023.12.17/rtx-v2023.12.17-linux-x64.tar.xz"
+      sha256 "b452d7eb29e5058a7452e58e9e5ec7cf03842c809727ac5deb1b7d9e10c1b4be"
     end
   end
 
   def install
     bin.install "bin/rtx"
     man1.install "man/man1/rtx.1"
-    generate_completions_from_executable(bin/"rtx", "completion")
+    generate_completions_from_executable(bin / "rtx", "completion")
     lib.mkpath
-    touch lib/".disable-self-update"
+    touch lib / ".disable-self-update"
   end
 
   test do
