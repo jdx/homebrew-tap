605ad49e901b5894aab7fe49df6cd1c29128157c
jdx <jdx@users.noreply.github.com>
2024-09-28 21:41:33 +0000

    usage 0.8.1

--- usage.rb
+++ usage.rb
@@ -1,8 +1,8 @@
 class Usage < Formula
   desc "Tool for CLIs"
   homepage "https://github.com/jdx/usage"
-  url "https://github.com/jdx/usage/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "dc54c47ee761a3e6ff31fbbb696b40e893b1fcdf2d2d15a96c488d8d57711097"
+  url "https://github.com/jdx/usage/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "0d80734b4b6952e2b79bc09b3f63a58781443fa824b389d16d87251662d79c6e"
   license "MIT"
   head "https://github.com/jdx/usage.git", branch: "main"
 
